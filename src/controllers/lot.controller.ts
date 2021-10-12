3b5572b306701d506477d8893d50560e0a9f359f
drh <jmnabangi@gmail.com>
2021-10-12 11:18:53 +0300

    implement getQuantity controller method
    Signed-off-by: drh <jmnabangi@gmail.com>

--- src/controllers/lot.controller.ts
+++ src/controllers/lot.controller.ts
@@ -1,5 +1,6 @@
 import {NextFunction, Request, Response} from 'express';
 import {sendErrResponse} from "../common/errors";
+import {Lot} from "../entity/Lot";
 
 
 // Retrieve Item quantity
@@ -8,13 +9,24 @@ const getQuantity = async (req: Request, res: Response) => {
 
     try {
         // Find all lots by item name
-
+        let lotsByItemName: Lot[] = await Lot.findNonExpired(item);
 
         // Perform quantity sum calculations
+        lotsByItemName = lotsByItemName.filter((lot) => lot.expiry > new Date());
 
+        // Do a summation of lot quantities
+        const sum = lotsByItemName.reduce((acc, val) => acc + val.quantity, 0);
+
+        // Get return object
+        lotsByItemName.sort((a, b) => a.expiry.valueOf() - b.expiry.valueOf());
+
+        const quantityObj =  {
+            quantity: sum,
+            validTill: sum > 0 ? lotsByItemName.reverse().pop()?.expiry.valueOf() : null
+        }
 
         // Send back data
-        res.status(200).send({});
+        res.status(200).send(quantityObj);
     } catch (e) {
         sendErrResponse(res, 500, (e as Error).message)
     }
