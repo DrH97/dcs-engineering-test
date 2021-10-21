0daad8cfe98fdcefe222f6f4efe3fae15c22d86b
drh <jmnabangi@gmail.com>
2021-10-21 11:40:47 +0300

    add clustering to improve concurrency
    Signed-off-by: drh <jmnabangi@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -7,12 +7,41 @@ const PORT: any = process.env.PORT || 3000;
 
 const app = new App(PORT);
 
-createConnection()
-  .then(() => {
-    /** Server */
-    app.listen();
-  })
-  .catch((e) => {
-    logger(e);
-    console.log("Could not connect to database! Check configs or connection.");
+
+import cluster from "cluster"
+import { cpus } from 'os';
+const totalCores = cpus().length;
+
+const start = () => {
+
+  createConnection()
+    .then(() => {
+      /** Server */
+      app.listen();
+    })
+    .catch((e) => {
+      logger(e);
+      console.log("Could not connect to database! Check configs or connection.");
+    });
+
+
+};
+
+if (cluster.isMaster) {
+  console.log(`Number of Cores is ${totalCores}`);
+  console.log(`Master ${process.pid} is running`);
+
+  // Fork workers.
+  for (let i = 0; i < totalCores; i++) {
+    cluster.fork();
+  }
+
+  cluster.on("exit", (worker) => {
+    console.log(`worker ${worker.process.pid} died`);
+    console.log("Let's fork another worker!");
+    cluster.fork();
   });
+
+} else {
+  start();
+}
